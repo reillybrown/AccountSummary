2406f1e521c74823feb590668cd900f411ef0755
Reilly Brown <reillybrown@me.com>
2020-10-25 13:00:37 -0400

    Add statement state object and getter for sorted transactions

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,16 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    statement: {}
+  },
+  getters: {
+    allTransactions: state => {
+      return state.statement.Transactions.sort((a, b) =>
+        a.TransactionDate < b.TransactionDate ? -1 : 1
+      );
+    }
+  },
   mutations: {},
   actions: {},
   modules: {}
