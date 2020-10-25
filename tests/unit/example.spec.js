c007ab753325a70d8726be304d02dcf31c3a6c14
Reilly Brown <reillybrown@me.com>
2020-10-25 13:19:38 -0400

    Remove HelloWorld component

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,11 +1,11 @@
 import { expect } from "chai";
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import AccountSummary from "@/components/AccountSummary.vue";
 
-describe("HelloWorld.vue", () => {
+describe("AccountSummary.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(AccountSummary, {
       propsData: { msg }
     });
     expect(wrapper.text()).to.include(msg);
